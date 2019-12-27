59af78055841ba36c842629ffb517e40a6cb2f21
Lucas <lucas_ssilva@outlook.com>
2019-12-27 14:25:06 -0300

    carga inicial

---
+++ execp/Entities/Reservation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using execp.Entities.Exceptions;
+
+namespace execp.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if(checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public int Duration()
+        {
+            return Checkout.Day - Checkin.Day;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime date = DateTime.Now;
+            if (checkin < date || checkout < date)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber + ", check-in: " + Checkin.ToString("dd/MM/yyyy") + ", check-out: " + Checkout.ToString("dd/MM/yyyy") + ", " + Duration() + " nights";
+        }
+    }
+}
---
+++ execp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using execp.Entities;
+using System.Collections.Generic;
+using execp.Entities.Exceptions;
+
+namespace execp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                Reservation r = new Reservation(room, checkin, checkout);
+
+                Console.WriteLine(r);
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation: ");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+                r.UpdateDates(checkin, checkout);
+                Console.WriteLine(r);
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Erro! \n\r" + e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Erro ! \n\r " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("try Again");
+            }
+        }
+
+
+    }
+}
+
